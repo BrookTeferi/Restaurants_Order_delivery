4ffa31670edb9c0631ce374fecc3c66643fe6db3
Brook Teferi <brookteferi1q2w@gmail.com>
2022-10-09 14:07:14 +0300

    cuss

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from customer.models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_display=['name','email','contact','date_created']
+    
+    
--- registers/urls.py
+++ registers/urls.py
@@ -11,10 +11,6 @@ urlpatterns = [
     path('register/',views.SignupView,name="register"),
     path('logout/',views.UserLogout.as_view(),name="logout"),
     path('profile/',views.UserProfile,name="user_view"),
-
-    # path('dash/<int:cid>/',views.dashboard_data,name = "dash-price-data")
-  
-    # path('logout/',a)
    
     
 ]
