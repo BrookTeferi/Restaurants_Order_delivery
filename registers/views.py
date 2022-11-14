a31b934c8305656fc3ecb955590123b6d25fa105
Brook Teferi <brookteferi1q2w@gmail.com>
2022-11-14 14:50:13 +0300

    ord

--- registers/views.py
+++ registers/views.py
@@ -53,7 +53,7 @@ def dashboard(request):
 	#                                 date_created__day = today_date.day).count()
 	
 	today_customers = customers.filter(date_created__gte = datetime.now() - timedelta(days=1)).count()#details of last 24 hours#b4 i also get same output using above line but now not so use this concept
-	today_order = orders.filter(created_at__year = today_date.year,created_at__month = today_date.month,created_at__day = today_date.day)
+	# today_order = orders.filter(created_at__year = today_date.year,created_at__month = today_date.month,created_at__day = today_date.day)
 	today_order = orders.filter(created_at__gte = datetime.now() - timedelta(days=1))#A timedelta object represents a duration, the difference between two dates or times.
 
 	order_total_price=0.00
